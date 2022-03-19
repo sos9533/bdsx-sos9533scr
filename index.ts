890a40cb788aa67e2746a2d98966ab161fb7de10
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-03-19 21:42:42 +0900

    add sos9533scr
    add sos9533scr.ts, index.ts

---
+++ index.ts
@@ -0,0 +1,9 @@
+import "./sos9533scr"
+
+echo "# sos9533scr" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sos9533/sos9533scr.git
+git push -u origin main
