ecfd7eac6ffdd4e38157b920e62fa31791851065
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-03-20 15:45:52 +0900

    fix loading

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-import "./sos9533scr"
+import { events } from "bdsx/event";
+import { bedrockServer } from "bdsx/launcher";
+
+if (bedrockServer.isLaunched()) {
+    import("./sos9533scr");
+} else {
+    events.serverOpen.on(() => {
+        import("./sos9533scr");
+    });
+}
