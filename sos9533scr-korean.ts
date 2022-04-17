35dc392f5365702961d7fd9bede673c8946983a8
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-04-17 10:58:49 +0900

    Update sos9533scr-korean.ts

--- sos9533scr-korean.ts
+++ sos9533scr-korean.ts
@@ -327,8 +327,6 @@ events.packetAfter(MinecraftPacketIds.Login).on((ptr, networkIdentifier, packetI
             kick(networkIdentifier, longnicknamekicktitle);
             console.log("\x1b[41m", `${username} kicked > [ Kicked by long nickname ]`, "\x1b[0m");
             bedrockServer.executeCommand(`tellraw @a {"rawtext":[{"text":"§l§f[ §esos9533scr §f]§r ${longnicknamekickmessage}"}]}`);
-        } else {
-            console.log(green(`${username}> IP:${ip}, XUID:${xuid} OS:${BuildPlatform[connreq.getDeviceOS()] || "UNKNOWN"}`));
         }
     }
 
