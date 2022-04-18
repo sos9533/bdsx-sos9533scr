7d2452535784a31a153dcee90c36c6c8c8fea770
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-04-18 23:05:16 +0900

    Fix 'cool' bug

--- sos9533scr-korean.ts
+++ sos9533scr-korean.ts
@@ -1052,7 +1052,7 @@ const cool = setInterval(() => {
     bedrockServer.executeCommand(`scoreboard players set @a cps 0`);
 }, 1000);
 
-events.serverLeave.on(() => {
+events.ServerClose.on(() => {
     clearInterval(cool);
 });
 
