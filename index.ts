ff633d4911eef5545505457201c47b9148ed0887
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-04-28 00:53:09 +0900

    ;

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 //what is your language / 당신의 언어는 무엇인가요
 //english / korean
 
-let language = "english"
+let language = "english";
 
 //한국인이시라면 sos9533scr-korean.ts 내부를 편집하여 사용해주세요.
 //If you use English, please edit sos9533scr-english.ts and use.
