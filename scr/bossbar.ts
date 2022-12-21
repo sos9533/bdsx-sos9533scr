f85a81edd23e8a384e78a7cc704aba9c63bf80ee
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-12-21 19:55:27 +0900

    update for `sos9533scr 2.0`

---
+++ scr/bossbar.ts
@@ -0,0 +1,50 @@
+import { CommandPermissionLevel, PlayerCommandSelector } from "bdsx/bds/command";
+import { BossEventPacket } from "bdsx/bds/packets";
+import { ServerPlayer } from "bdsx/bds/player";
+import { command } from "bdsx/command";
+import { CxxString, float32_t } from "bdsx/nativetype";
+import { RemoveBossBarCommand, SetBossBarCommand } from "../setting";
+import { serverProperties } from "bdsx/serverproperties";
+const levelname = serverProperties["level-name"]
+
+command.register(RemoveBossBarCommand, "remove bossbar", CommandPermissionLevel.Operator).overload(
+    (params, origin, output) => {
+        for (const target of params.target.newResults(origin, ServerPlayer)) {
+            target.removeBossBar();
+        }
+    },
+    {
+        target: PlayerCommandSelector,
+        title: CxxString,
+    },
+);
+
+command.register(SetBossBarCommand, "set bossbar", CommandPermissionLevel.Operator).overload(
+    (params, origin, output) => {
+        for (const target of params.target.newResults(origin, ServerPlayer)) {
+            let color: BossEventPacket.Colors = BossEventPacket.Colors.Purple;
+            if (params.enum === "blue") {
+                color = BossEventPacket.Colors.Blue;
+            } else if (params.enum === "red") {
+                color = BossEventPacket.Colors.Red;
+            } else if (params.enum === "green") {
+                color = BossEventPacket.Colors.Green;
+            } else if (params.enum === "yellow") {
+                color = BossEventPacket.Colors.Yellow;
+            } else if (params.enum === "purple") {
+                color = BossEventPacket.Colors.Purple;
+            } else if (params.enum === "white") {
+                color = BossEventPacket.Colors.White;
+            }
+            target.setBossBar(params.title, params.percent, color);
+        }
+    },
+    {
+        target: PlayerCommandSelector,
+        title: CxxString,
+        percent: float32_t,
+        enum: command.enum("color", "blue", "red", "green", "yellow", "purple", "white"),
+    },
+);
+
+console.info("[ " + "sos9533scr".yellow + " ] " + `${levelname}`.red +` - bossbar.ts loaded`.gray)
