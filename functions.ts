f65c4735f64bd73602408613a42d83fa7c965470
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-12-21 19:51:15 +0900

    update for `sos9533scr 2.0`

---
+++ functions.ts
@@ -0,0 +1,48 @@
+import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
+import { CANCEL } from "bdsx/common";
+import { bedrockServer } from "bdsx/launcher";
+import * as fs from "fs";
+
+export function kick(target: NetworkIdentifier, message: string): CANCEL {
+    bedrockServer.serverInstance.disconnectClient(target, message);
+    return CANCEL;
+};
+
+export function makeFile(filepath: string, value = {}) {
+    if (!fs.existsSync(filepath)) {
+        fs.writeFileSync(filepath, JSON.stringify(value));
+        console.log("[", "sos9533scr".yellow, "]", `Made '${filepath}'`.gray, " - sos9533".green);
+    }
+}
+
+export function makeDir(dirname: string) {
+    if (!fs.existsSync(dirname)) {
+        fs.mkdirSync(dirname);
+        console.log("[", "sos9533scr".yellow, "]", `Made '${dirname}'`.gray, " - sos9533".green);
+    }
+}
+
+export function leadZero(num: number, n: number) {
+    var leadZero = "";
+    let num2 = num.toString();
+    if (num2.length < n) {
+        for (var i = 0; i < n - num2.length; i++) leadZero += "0";
+    }
+    return leadZero + num;
+}
+
+export function dateWithZero() {
+    var d = new Date();
+    return (
+        d.getFullYear() +
+        "-" +
+        leadZero(d.getMonth() + 1, 2) +
+        "-" +
+        leadZero(d.getDate(), 2) +
+        "-" +
+        leadZero(d.getHours(), 2) +
+        "-" +
+        leadZero(d.getMinutes(), 2) +
+        "-"
+    );
+}
