589aadfb73246207708ca855bbcb9e21a0dc20b2
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-12-21 19:49:22 +0900

    update for `sos9533scr 2.0`

--- index.ts
+++ index.ts
@@ -1,25 +1,61 @@
 require("./bds");
+require("./scr");
 
 import { events } from "bdsx/event";
 import { bedrockServer } from "bdsx/launcher";
+import { serverProperties } from "bdsx/serverproperties";
 
-function isAbstracted(obj: any): boolean {
+export function isAbstracted(obj: any): boolean {
     try {
         Object.isExtensible(obj);
         return false;
     } catch (err) {
         if (err.message === "BDS is not loaded yet") {
             return true;
+        } else if (err.message === "bedrock_server is not launched yet") {
+            return true;
         }
         throw err;
     }
 }
 
+const levelname = serverProperties["level-name"]
+
 if (!isAbstracted(bedrockServer.minecraft)) {
-    import("./sos9533scr");
+    console.info("[ " + "sos9533scr".yellow + " ] " + `${levelname}`.red +` - Plugin Strat Loading...`.gray)
+    import("./scr/anticrasher");
+    import("./scr/ban");
+    import("./scr/bossbar");
+    import("./scr/exit");
+    import("./scr/home");
+    import("./scr/info");
+    import("./scr/item");
+    import("./scr/join");
+    import("./scr/kick");
+    import("./scr/message");
+    import("./scr/mute");
+    import("./scr/prefix");
+    import("./scr/timecommand");
+    import("./scr/tpcommand");
+    import("./scr/tpa");
 } else {
     events.serverOpen.on(() => {
-        import("./sos9533scr");
+        console.info("[ " + "sos9533scr".yellow + " ] " + `${levelname}`.red +` - Plugin Strat Loading...`.gray)
+        import("./scr/anticrasher");
+        import("./scr/ban");
+        import("./scr/bossbar");
+        import("./scr/exit");
+        import("./scr/home");
+        import("./scr/info");
+        import("./scr/item");
+        import("./scr/join");
+        import("./scr/kick");
+        import("./scr/message");
+        import("./scr/mute");
+        import("./scr/prefix");
+        import("./scr/timecommand");
+        import("./scr/tpcommand");
+        import("./scr/tpa");
     });
 }
 
