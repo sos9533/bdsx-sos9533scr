054d800ba86a403868d1eabcd6ee2527adc2c0ce
sos9533 <62497219+sos9533@users.noreply.github.com>
2022-12-27 13:05:03 +0900

    add `addlog`

--- scr/exit.ts
+++ scr/exit.ts
@@ -2,12 +2,14 @@ import { events } from "bdsx/event";
 import { playerList } from "./join";
 import { red } from "colors";
 import { serverProperties } from "bdsx/serverproperties";
+import { addlog } from "./log";
 const levelname = serverProperties["level-name"]
 
 events.networkDisconnected.on((networkIdentifier) => {
     const username = playerList.get(networkIdentifier);
     playerList.delete(networkIdentifier);
     console.log(red(`${username}> Exit server`));
+    addlog(`${username}> Exit server`)
 });
 
 console.info("[ " + "sos9533scr".yellow + " ] " + `${levelname}`.red +` - exit.ts loaded`.gray)
