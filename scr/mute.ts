6f6dda7647e13be18d81e6037f518b5068349be4
sos9533 <62497219+sos9533@users.noreply.github.com>
2023-02-12 17:20:50 +0900

    Add `Translate`

--- scr/mute.ts
+++ scr/mute.ts
@@ -2,24 +2,18 @@ import { CommandPermissionLevel, PlayerCommandSelector } from "bdsx/bds/command"
 import { ServerPlayer } from "bdsx/bds/player";
 import { command } from "bdsx/command";
 import { serverProperties } from "bdsx/serverproperties";
-import { language, MuteCommand, runCommand, SystemMessageTitle, UnmuteCommand } from "../setting";
+import { Translate } from "..";
+import { MuteCommand, runCommand, SystemMessageTitle, UnmuteCommand } from "../setting";
 import { addlog } from "./log";
 const levelname = serverProperties["level-name"];
 
-command.register(MuteCommand, "mute player", CommandPermissionLevel.Operator).overload(
+command.register(MuteCommand, Translate("command.MuteExplanation"), CommandPermissionLevel.Operator).overload(
     (param, origin, output) => {
         for (const target of param.target.newResults(origin, ServerPlayer)) {
             const username = target.getNameTag();
 
             runCommand(`tag @a[name="${username}"] add mute`);
-            if (language === "english") {
-                runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §7${username}§f is §cmuted§f by ${origin.getName()}"}]}`);
-            }
-            if (language === "korean") {
-                runCommand(
-                    `tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §7${username}§f님이 ${origin.getName()}님에 의해 §c채팅금지§f 조치 되었습니다."}]}`,
-                );
-            }
+            runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §7${username}§f ${Translate("mute.MuteSuccess")} ${origin.getName()}"}]}`);
 
             console.log("\x1b[41m", `${username} Mute > [ Muted by ${origin.getName()} ]`, "\x1b[0m");
             addlog(`${username} Muted > [ Muted by ${origin.getName()} ]`);
@@ -30,20 +24,14 @@ command.register(MuteCommand, "mute player", CommandPermissionLevel.Operator).ov
     },
 );
 
-command.register(UnmuteCommand, "unmute player", CommandPermissionLevel.Operator).overload(
+command.register(UnmuteCommand, Translate("command.UnMuteExplanation"), CommandPermissionLevel.Operator).overload(
     (param, origin, output) => {
         for (const target of param.target.newResults(origin, ServerPlayer)) {
             const username = target.getNameTag();
 
-            if (language === "english") {
-                runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §7${username}§f is §aunmuted§f by ${origin.getName()}"}]}`);
-            }
-            if (language === "korean") {
-                runCommand(
-                    `tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §7${username}§f님이 ${origin.getName()}님에 의해 §a채팅금지 해제§f 조치 되었습니다."}]}`,
-                );
-            }
+            runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §7${username}§f ${Translate("mute.UnMuteSuccess")} ${origin.getName()}"}]}`);
             runCommand(`tag @a[name="${username}"] remove mute`);
+            
             console.log("\x1b[41m", `${username} UnMute > [ UnMuted by ${origin.getName()} ]`, "\x1b[0m");
             addlog(`${username} unMuted > [ unMuted by ${origin.getName()} ]`);
         }
