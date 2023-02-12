7042e08ee7684fed60396ffcf50170537c95598f
sos9533 <62497219+sos9533@users.noreply.github.com>
2023-02-12 17:19:45 +0900

    Add `Translate`

--- scr/kick.ts
+++ scr/kick.ts
@@ -2,22 +2,20 @@ import { CommandPermissionLevel, PlayerCommandSelector } from "bdsx/bds/command"
 import { ServerPlayer } from "bdsx/bds/player";
 import { command } from "bdsx/command";
 import { serverProperties } from "bdsx/serverproperties";
+import { Translate } from "..";
 import { kick } from "../functions";
-import { KickCommand, KickTitle, language, runCommand, SystemMessageTitle } from "../setting";
+import { KickCommand, runCommand, SystemMessageTitle } from "../setting";
 const levelname = serverProperties["level-name"];
 
-command.register(KickCommand, "kick player", CommandPermissionLevel.Operator).overload(
+command.register(KickCommand, Translate("command.KickExplanation"), CommandPermissionLevel.Operator).overload(
     (param, origin, output) => {
         for (const target of param.target.newResults(origin, ServerPlayer)) {
             const username = target.getNameTag();
             const ni = target.getNetworkIdentifier();
+
+            const KickTitle = Translate("kick.KickTitle")
             kick(ni, KickTitle);
-            if (language === "english") {
-                runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §c${username}§f is kicked by ${origin.getName()}"}]}`);
-            }
-            if (language === "korean") {
-                runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §c${username}§f님이 ${origin.getName()}님에 의해 강제퇴장 조치 되었습니다."}]}`);
-            }
+            runCommand(`tellraw @a {"rawtext":[{"text":"${SystemMessageTitle} §c${username}§f ${Translate("kick.KickMessage")} ${origin.getName()}"}]}`);
 
             console.log("\x1b[41m", `${username} kicked > [ Kicked by ${origin.getName()} ]`, "\x1b[0m");
         }
